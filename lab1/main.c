680a12fcb223fe37933f875be783701fe936eeb7
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2018-03-08 13:52:48 +0000

    Add colours to the boring box

--- lab1/main.c
+++ lab1/main.c
@@ -5,9 +5,13 @@ void display(void) {
 
   glBegin(GL_POLYGON);
   {
+    glColor3f(1.0, 0.0, 0.0);
     glVertex2f(-0.5, -0.5);
+    glColor3f(0.0, 1.0, 0.0);
     glVertex2f(-0.5, 0.5);
+    glColor3f(0.0, 0.0, 1.0);
     glVertex2f(0.5, 0.5);
+    glColor3f(1.0, 1.0, 0.0);
     glVertex2f(0.5, -0.5);
   }
   glEnd();
